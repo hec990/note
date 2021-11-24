a88801040b9b5b2978b9b556af5db4d7d5985605
xiaohh <1296522554@qq.com>
2021-11-24 22:11:15 +0800

    完成注册账户和登录的验证规则

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -7,19 +7,21 @@
           <div class="form">
             <h3 @click="showRegister()">创建账户</h3>
             <transition name="slide">
-            <div v-bind:class="{show: isShowRegister}" class="register">
-              <input type="text" placeholder="用户名">
-              <input type="password" placeholder="密码">
-              <div class="button">创建账号</div>
-            </div>
+              <div v-bind:class="{show: isShowRegister}" class="register">
+                <input type="text" v-model="register.username" @keyup.enter="onRegister" placeholder="用户名">
+                <input type="password" v-model="register.password" @keyup.enter="onRegister" placeholder="密码">
+                <p v-bind:class="{error: register.isError}"> {{ register.notice }}</p>
+                <div class="button" @click="onRegister">创建账号</div>
+              </div>
             </transition>
             <h3 @click="showLogin()">登录</h3>
             <transition name="slide">
-            <div v-bind:class="{show: isShowLogin}" class="login">
-              <input type="text" placeholder="输入用户名">
-              <input type="password" placeholder="密码">
-              <div class="button"> 登录</div>
-            </div>
+              <div v-bind:class="{show: isShowLogin}" class="login">
+                <input type="text" v-model="login.username" @keyup.enter="onLogin" placeholder="输入用户名" >
+                <input type="password" v-model="login.password" @keyup.enter="onLogin" placeholder="密码" >
+                <p v-bind:class="{error: login.isError}"> {{ login.notice }}</p>
+                <div class="button" @click="onLogin"> 登录</div>
+              </div>
             </transition>
           </div>
         </div>
@@ -37,6 +39,18 @@ export default {
       isShowRegister: false,
       // 是否显示登录
       isShowLogin: true,
+      register: {
+        username: '',
+        password: '',
+        notice: "创建账号后，请记住用户名和密码",
+        isError: false
+      },
+      login: {
+        username: '',
+        password: '',
+        notice: "输入用户名和密码",
+        isError: false
+      }
     }
   },
   methods: {
@@ -47,6 +61,34 @@ export default {
     showLogin() {
       this.isShowLogin = true;
       this.isShowRegister = false;
+    },
+    onRegister() {
+      if (!/^[\w\u4e00-\u9fa5]{3,15}$/.test(this.register.username)) {
+        this.register.isError = true
+        this.register.notice = '用户名3~15个字符，仅限于字母数字下划线中文'
+        return
+      }
+      if (!/^.{6,16}$/.test(this.register.password)) {
+        this.register.isError = true
+        this.register.notice = '密码长度为6~16个字符'
+        return
+      }
+      this.register.isError = false
+      this.register.notice = ''
+    },
+    onLogin(){
+      if (!/^[\w\u4e00-\u9fa5]{3,15}$/.test(this.login.username)) {
+        this.login.isError = true
+        this.login.notice = '用户名3~15个字符，仅限于字母数字下划线中文'
+        return
+      }
+      if (!/^.{6,16}$/.test(this.login.password)) {
+        this.login.isError = true
+        this.login.notice = '密码长度为6~16个字符'
+        return
+      }
+      this.login.isError = false
+      this.login.notice = ''
     }
   }
 };
