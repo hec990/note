76bf33f270b25d28066e1fcb2cfc55c0598cb5e9
xiaohh <1296522554@qq.com>
2021-11-24 20:24:23 +0800

    支持注册账户和登录切换了

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -5,14 +5,14 @@
         <div class="modal-container">
           <div class="main"></div>
           <div class="form">
-            <h3>创建账户</h3>
-            <div v-show="true" class="register">
+            <h3 @click="showRegister()">创建账户</h3>
+            <div v-show="isShowRegister" class="register">
               <input type="text" placeholder="用户名">
               <input type="password" placeholder="密码">
               <div class="button">创建账号</div>
             </div>
-            <h3>登录</h3>
-            <div v-show="false" class="login">
+            <h3 @click="showLogin()">登录</h3>
+            <div v-show="isShowLogin" class="login">
               <input type="text" placeholder="输入用户名">
               <input type="password" placeholder="密码">
               <div class="button"> 登录</div>
@@ -26,7 +26,25 @@
 
 <script lang="js">
 export default {
-  name: "Login"
+  name: "Login",
+  data() {
+    return {
+      // 是否显示注册账户
+      isShowRegister: false,
+      // 是否显示登录
+      isShowLogin: true,
+    }
+  },
+  methods: {
+    showRegister() {
+      this.isShowRegister = true;
+      this.isShowLogin = false;
+    },
+    showLogin() {
+      this.isShowLogin = true;
+      this.isShowRegister = false;
+    }
+  }
 };
 </script>
 
