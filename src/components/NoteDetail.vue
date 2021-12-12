795dd2445dd374a8edbd94cc2a21f9f1b7b04d26
xiaohh <1296522554@qq.com>
2021-12-12 21:29:49 +0800

    完成笔记本详情静态页面的开发

--- src/components/NoteDetail.vue
+++ src/components/NoteDetail.vue
@@ -1,18 +1,53 @@
 <template>
-  <div id="note-detail">
-    <h1>notebookId:{{$route.query.notebookId}}</h1>
-    <h1>noteId:{{$route.query.noteId}}</h1>
+  <div id="note" class="detail">
+    <note-sidebar/>
+    <div class="note-detail">
+      <div class="note-empty" v-show="!curNote.id">请选择笔记</div>
+      <div class="note-detail-ct" v-show="curNote.id">
+        <div class="note-bar">
+          <span> 创建日期: {{ curNote.createdAtFriendly }}</span>
+          <span> 更新日期: {{ curNote.updatedAtFriendly }}</span>
+          <span> {{ statusText }}</span>
+          <span>
+            <svg class="icon">
+                <use xlink:href="#icon-xianshi"></use>
+            </svg>
+          <svg class="icon">
+            <use xlink:href="#icon-lajitong"></use>
+          </svg>
+          </span>
+        </div>
+        <div class="note-title">
+          <input type="text"
+                 placeholder="输入标题" />
+        </div>
+        <div class="editor">
+          <textarea
+              placeholder="输入内容, 支持 markdown 语法">
+          </textarea>
+          <div class="preview markdown-body"></div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="js">
 import Auth from "@/apis/auth";
+import NoteSidebar from "@/components/NoteSidebar";
 
 export default {
   name: "NoteDetail",
+  components: {NoteSidebar},
   data () {
     return {
-      msg: '笔记详情页'
+      msg: '笔记详情页',
+      curNote:{
+        id:1,
+        createdAtFriendly:"2021年12月11日23:02:45",
+        updatedAtFriendly:"2021年12月11日23:12:15"
+      },
+      statusText:"正在输入中"
     }
   },
   created() {
@@ -27,5 +62,75 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.icon {
+  width: 2em;
+  height:2em;
+}
+#note {
+  display: flex;
+  align-items: stretch;
+  background-color: #fff;
+  flex: 1;
+  .note-detail {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    .note-empty {
+      font-size: 50px;
+      color: #ccc;
+      text-align: center;
+      margin-top: 100px;
+    }
+    .note-detail-ct {
+      height: 100%;
+      .note-bar {
+        padding: 4px 20px;
+        border-bottom: 1px solid #eee;
+        &:after {
+          content:'';
+          display: block;
+          clear: both;
+        }
+        span {
+          font-size: 12px;
+          color: #999;
+          margin-right: 4px;
+          &:last-child{
+            float: right;
+            svg:first-child{
+              margin-right: 20px;
+            }
+          }
+        }
+      }
+      .note-title{
+        input {
+          display: inline-block;
+          width: 100%;
+          border: none;
+          outline: none;
+          font-size: 18px;
+          padding: 10px 20px;
+        }
+      }
+      .editor {
+        height: calc(100% - 70px);
+        position: relative;
+        textarea, .preview {
+          position: absolute;
+          width: 100%;
+          height: 100%;
+          padding: 20px;
+        }
+        textarea {
+          border: none;
+          resize: none;
+          outline: none;
+          font-size: 14px;
+          font-family: 'Monaco', courier, monospace;
+        }
+      }
+    }
+  }
+}
 </style>
---
+++ src/components/NoteSidebar.vue
@@ -0,0 +1,154 @@
+<template>
+  <div class="note-sidebar">
+    <span class="btn add-note" @click="onAddNote">添加笔记</span>
+    <el-dropdown class="notebook-title" @command="handleCommand" placement="bottom">
+      <span class="el-dropdown-link">
+        {{ curBook.title }}
+        <svg class="arrow-icon">
+            <use xlink:href="#icon-bottom"></use>
+          </svg>
+      </span>
+      <!--  每一项 -->
+      <el-dropdown-menu slot="dropdown">
+        <el-dropdown-item v-for="(notebook,index) in notebooks" :key="index" :command="notebook.id">
+          {{ notebook.title }}
+        </el-dropdown-item>
+        <el-dropdown-item command="trash">回收站</el-dropdown-item>
+      </el-dropdown-menu>
+    </el-dropdown>
+    <div class="menu">
+      <div>更新时间</div>
+      <div>标题</div>
+    </div>
+    <ul class="notes">
+      <li v-for="(note,index) in notes" :key="index">
+        <router-link :to="`/note?noteId=${note.id}&notebookId=${curBook.id}`">
+          <span class="date">{{ note.updatedAtFriendly }}</span>
+          <span class="title">{{ note.title }}</span>
+        </router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="js">
+export default {
+  name: "NoteSidebar",
+  data() {
+    return {
+      notebooks: [],
+      notes: [],
+      curBook: {
+        title: "前端学习笔记"
+      }
+    }
+  },
+  methods: {
+    onAddNote() {
+      console.log('添加笔记')
+    },
+    handleCommand() {
+      console.log('被点击了')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.arrow-icon {
+  width: 1em;
+  height: 1em;
+}
+
+.icon {
+  width: 1em;
+  height: 1em;
+}
+
+.note-sidebar {
+  position: relative;
+  width: 290px;
+  border-right: 1px solid #ccc;
+  background-color: #eee;
+  min-height: 100%;
+
+  .add-note {
+    position: absolute;
+    right: 5px;
+    top: 12px;
+    color: #666;
+    font-size: 12px;
+    padding: 2px 4px;
+    box-shadow: 0 0 2px 0 #ccc;
+    border: none;
+    z-index: 1;
+  }
+
+  .notebook-title {
+    font-size: 18px;
+    font-weight: normal;
+    color: #333;
+    height: 45px;
+    line-height: 45px;
+    text-align: center;
+    border-bottom: 1px solid #ccc;
+    background-color: #f7f7f7;
+    display: block;
+  }
+
+  .el-dropdown-link {
+    cursor: pointer;
+  }
+
+  .el-dropdown-menu__item {
+    width: 200px;
+  }
+
+  .menu {
+    display: flex;
+
+    div {
+      font-size: 12px;
+      padding: 2px 10px;
+      flex: 1;
+      border-right: 1px solid #ccc;
+      border-bottom: 1px solid #ccc;
+
+      &:last-child {
+        border-right: none;
+      }
+    }
+
+    .iconfont {
+      font-size: 10px;
+    }
+  }
+
+  .notes {
+    li {
+
+      &:nth-child(odd) {
+        background-color: #f2f2f2;
+      }
+
+      a {
+        display: flex;
+        padding: 3px 0;
+        font-size: 12px;
+        border: 2px solid transparent;
+      }
+
+      .router-link-exact-active {
+        border: 2px solid #81c0f3;
+        border-radius: 3px;
+      }
+
+      span {
+        padding: 0 10px;
+        flex: 1;
+      }
+    }
+  }
+}
+
+</style>
