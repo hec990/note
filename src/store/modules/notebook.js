f48a2da5c7d43d8d4c00ce839b90594360369d25
xiaohh <1296522554@qq.com>
2022-01-01 23:03:24 +0800

    全局数据管理抽离出一个notebook模块

---
+++ src/store/modules/notebook.js
@@ -0,0 +1,18 @@
+// 数据源
+const state = {}
+
+// 计算属性
+const getters = {}
+
+// 同步方法
+const mutations = {}
+
+//异步方法
+const actions = {}
+
+export default {
+    state,
+    getters,
+    mutations,
+    actions
+}
